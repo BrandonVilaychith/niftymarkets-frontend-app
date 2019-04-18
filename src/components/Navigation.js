97ddab70a91b19e4d046e2ceb5ee670a3187a4d8
Brandon Vilaychith <blvilaychith@gmail.com>
2019-04-17 23:49:25 -0500

    Created basic navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import Logo from "../images/Logo.png";
+import { Link } from "react-router-dom";
+
+import {
+	Collapse,
+	Navbar,
+	NavbarToggler,
+	NavbarBrand,
+	Nav,
+	NavItem,
+	NavLink,
+	UncontrolledDropdown,
+	DropdownToggle,
+	DropdownMenu,
+	DropdownItem,
+} from "reactstrap";
+
+const Navigation = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggle = () => {
+		setIsOpen(!isOpen);
+	};
+
+	return (
+		<div>
+			<Navbar color='primary' light expand='md'>
+				<NavbarBrand href='/' className='text-white'>
+					<img src={Logo} alt='Logo' style={{ width: "30px" }} />
+				</NavbarBrand>
+				<NavbarToggler onClick={toggle} />
+				<Collapse isOpen={isOpen} navbar>
+					<Nav className='ml-auto' className='text-white' navbar>
+						<NavItem>
+							<NavLink href='/components/'>Home</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink
+								href='https://github.com/reactstrap/reactstrap'
+								className='text-white'>
+								Cart
+							</NavLink>
+						</NavItem>
+						<UncontrolledDropdown nav inNavbar>
+							<DropdownToggle nav caret className='text-white'>
+								Profile
+							</DropdownToggle>
+							<DropdownMenu right>
+								<DropdownItem>
+									<Link to='/profile'>My Page</Link>
+								</DropdownItem>
+								{/* <DropdownItem divider /> */}
+								<DropdownItem>My Items</DropdownItem>
+								{/* <DropdownItem divider /> */}
+								<DropdownItem>Reset</DropdownItem>
+							</DropdownMenu>
+						</UncontrolledDropdown>
+					</Nav>
+				</Collapse>
+			</Navbar>
+		</div>
+	);
+};
+
+export default Navigation;
